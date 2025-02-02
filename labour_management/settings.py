4ee49a97eac748efface571a49ae467dff1dee99
Deepesh Patel <deepeshpatel28@icloud.com>
2025-02-02 17:01:35 +0000

    Configured AWS RDS PostgreSQL, applied migrations, and secured credentials with env.py

--- labour_management/settings.py
+++ labour_management/settings.py
@@ -11,6 +11,12 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+import sys
+
+# Load environment variables from env.py
+sys.path.append(os.path.abspath(os.path.dirname(__file__)))
+import env  # noqa
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,10 +83,16 @@ WSGI_APPLICATION = 'labour_management.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get("DB_NAME"),
+        'USER': os.environ.get("DB_USER"),
+        'PASSWORD': os.environ.get("DB_PASSWORD"),
+        'HOST': os.environ.get("DB_HOST"),
+        'PORT': os.environ.get("DB_PORT"),
     }
 }
+
+
 
 
 # Password validation
